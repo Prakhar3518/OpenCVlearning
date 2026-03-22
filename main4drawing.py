0999a8fa28122aca0fd4885f5bfb019fb3d1a29b
Prakhar Chaudhary <prakharchaudhary0302@gmail.com>
2026-03-22 20:14:12 +0530

    Lines manipulation added

---
+++ main4drawing.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    lineimg = cv2.line(frame,(0,0),(width,height),(0,255,0),2)
+    lineimg = cv2.line(frame, (width, 0), (0,height), (0, 0, 255), 2)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
